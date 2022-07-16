e4e987f52f3eb9b2a1fbe659b568bcd09a36c80d
Pooja-Sarai <109147616+Pooja-Sarai@users.noreply.github.com>
2022-07-16 05:56:28 +0530

    Add files via upload

---
+++ socioweb_room.js
@@ -0,0 +1,56 @@
+
+//ADD YOUR FIREBASE LINKS
+var firebaseConfig = {
+  apiKey: "AIzaSyCxEGHktOlBWOtFxLAtKIliEACAtzNipIc",
+  authDomain: "carracinggame-4c404.firebaseapp.com",
+  databaseURL: "https://carracinggame-4c404-default-rtdb.firebaseio.com",
+  projectId: "carracinggame-4c404",
+  storageBucket: "carracinggame-4c404.appspot.com",
+  messagingSenderId: "880535245087",
+  appId: "1:880535245087:web:d05672bc783ee8a8b0e02a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "socioweb_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "socioweb_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
